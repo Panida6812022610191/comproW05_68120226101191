55802de3be5a624b7f3435e3e537396a45dfff7a
Panida <s6812022610191@email.kmutnb.ac.th>
2025-07-25 12:58:42 +0700

    รัก

--- coding_w05-02.c
+++ coding_w05-02.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
-    char First_Name[30] ;                                     // Variable to store the first character of the name
-    int age ;                                                 // Variable to store the age    
-    float hight;                                               // Variable to store the height
-    char subject[10];                                           // Variable to store the subject        
-    float grade;                                                // Variable to store the grade  
-    char gradeA;                                                // Variable to store the grade symbol                                                          
+    char First_Name[30] ;                                     //Char ประกาศตัวแปรชื่อ
+    int age ;                                                 //Int ประกาศตัวแปรอายุ
+    float hight;                                              //Float ประกาศตัวแปรส่วนสูง
+    char subject;                                             //Char ประกาศตัวแปรชื่อวิชา
+    float grade;                                              //Float ประกาศตัวแปรเกรด
+    char gradeA;                                              //Char ประกาศตัวแปรสัญลักษณ์เกรด                          
    
-    printf("Enter your First Name age and hight):\n");          //ใส่ชื่อ นามสกุล อายุ และส่วนสูง
-    scanf("%s %d %f", &First_Name,&age, &hight);                //รับค่าชื่อ นามสกุล อายุ และส่วนสูง
+    printf("Enter your First Name age and hight):\n");          //ใส่ชื่อ อายุ และส่วนสูง
+    scanf("%s %d %f", &First_Name,&age, &hight);                //รับค่าชื่อ อายุ และส่วนสูง
 
     printf("Enter your Subject, grade and symbolized:\n");      //ใส่วิชา เกรด และสัญลักษณ์เกรด
     scanf("%s %f %c", &subject, &grade, &gradeA);               //รับค่าชื่อวิชา เกรด และสัญลักษณ์เกรด
